a72d20ce70ecc0d8e364df5660a45d4fbffd9723
Bram Veenboer <bram.veenboer@gmail.com>
2024-03-19 17:11:34 +0100

    Add mma_kernel_ptx and example for 4-bit

--- kernels/mma.cu
+++ kernels/mma.cu
@@ -23,8 +23,28 @@ __device__ void mma_kernel(Tout* data) {
   store_matrix_sync(ptr, sum, N, mem_row_major);
 }
 
+#include "mma_m8n8k32_s32s4s4s32.cuh"
+
+template <typename Tin, typename Tout, unsigned M, unsigned N, unsigned K>
+__device__ void mma_kernel_ptx(Tout* data) {
+  fragment<accumulator, M, N, K, Tout> sum;
+  fragment<matrix_a, M, N, K, Tin, row_major> aFrag;
+  fragment<matrix_b, M, N, K, Tin, col_major> bFrag;
+
+  fill_fragment(sum, 0);
+  fill_fragment(aFrag, 0);
+  fill_fragment(bFrag, 0);
+
+  for (unsigned k = 0; k < REPEAT_COUNT; k++) {
+    mma_sync_ptx(sum, aFrag, bFrag, sum);
+  }
+
+  Tout* ptr = &data[threadIdx.y * M * N];
+  store_matrix_sync(ptr, sum, N, mem_row_major);
+}
+
 __global__ void mma4_kernel(void* data) {
-  mma_kernel<experimental::precision::s4, int, 8, 8, 32>((int*)data);
+  mma_kernel_ptx<experimental::precision::s4, int, 8, 8, 32>((int*)data);
 }
 
 __global__ void mma8_kernel(void* data) {
---
+++ kernels/mma_m8n8k32_s32s4s4s32.cuh
@@ -0,0 +1,17 @@
+#include <mma.h>
+
+using namespace nvcuda::wmma;
+
+inline __device__ void mma_sync_ptx(
+    fragment<accumulator, 8, 8, 32, int>& d,
+    const fragment<matrix_a, 8, 8, 32, experimental::precision::s4, row_major>&
+        a,
+    const fragment<matrix_b, 8, 8, 32, experimental::precision::s4, col_major>&
+        b,
+    const fragment<accumulator, 8, 8, 32, int>& c) {
+  asm volatile(
+      "mma.sync.aligned.row.col.m8n8k32.s32.s4.s4.s32 {%0, %1}, {%2}, {%3}, "
+      "{%4, %5};\n"
+      : "=r"(d.x[0]), "=r"(d.x[1])
+      : "r"(a.x[0]), "r"(b.x[0]), "r"(c.x[0]), "r"(c.x[1]));
+}
