9d5ec48b438644904ca8ba9bace15d4bbe42b0bd
Bram Veenboer <bram.veenboer@gmail.com>
2025-07-25 13:51:39 +0200

    Fix int32 benchmark for RTC

--- benchmarks/int32.cpp
+++ benchmarks/int32.cpp
@@ -1,9 +1,14 @@
 #include "common/common.h"
 
+#include "kernels/int32.cu.o.h"
+
 __global__ void int32_kernel(int *ptr);
 
 int main(int argc, const char *argv[]) {
   Benchmark benchmark(argc, argv);
+  KernelFactory kernel_factory(int32_source);
+  auto kernel =
+      kernel_factory.compileKernel(benchmark.getDevice(), "int32_kernel");
 
   // Parameters
   int multiProcessorCount = benchmark.multiProcessorCount();
@@ -25,8 +30,7 @@ int main(int argc, const char *argv[]) {
 
   // Run benchmark
   for (int i = 0; i < benchmark.nrBenchmarks(); i++) {
-    benchmark.run(reinterpret_cast<void *>(&int32_kernel), grid, block, "int32",
-                  gops, gbytes);
+    benchmark.run(kernel, grid, block, "int32", gops, gbytes);
   }
 
   return EXIT_SUCCESS;
