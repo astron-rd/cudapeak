95a8216406f32b3173435829c8c0588b7aedb50d
Bram Veenboer <bram.veenboer@gmail.com>
2025-07-29 13:26:38 +0200

    Add m16n8k32 int8 kernel to mma benchmark

--- kernels/mma_m16n8k32_f32f6f6f32.cuh
+++ kernels/mma_m16n8k32_f32f6f6f32.cuh
@@ -18,7 +18,8 @@ mma_sync_ptx(fragment<accumulator, 16, 8, 32, float> &d,
              const fragment<matrix_b, 16, 8, 32, __nv_fp6_e2m3, col_major> &b,
              const fragment<accumulator, 16, 8, 32, float> &c) {
 
-  asm("mma.sync.aligned.m16n8k32.row.col.kind::f8f6f4.f32.e3m2.e2m3.f32 {%0, %1, %2, %3}, "
+  asm("mma.sync.aligned.m16n8k32.row.col.kind::f8f6f4.f32.e3m2.e2m3.f32 {%0, "
+      "%1, %2, %3}, "
       "{%4, %5, %6, %7}, {%8, %9}, {%10, %11, %12, %13};"
       : "=f"(d.x[0]), "=f"(d.x[1]), "=f"(d.x[2]), "=f"(d.x[3])
       : "r"(a.x[0]), "r"(a.x[1]), "r"(a.x[2]), "r"(a.x[3]), "r"(b.x[0]),
